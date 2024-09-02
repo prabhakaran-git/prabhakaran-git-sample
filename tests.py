1572fc30ad41a9b44a8379f1852b56a641a285bf
Prabhakaran Mathivanan <prabha393@gmail.com>
2024-09-02 19:50:34 +0530

    Adding tests

---
+++ tests.py
@@ -0,0 +1,11 @@
+def test_addition():
+  assert 5 + 5 == 10
+
+def test_subtraction():
+  assert 10 - 5 == 5
+
+def test_multiplication():
+  assert 10 * 5 == 50
+
+def test_division():
+  assert 10 // 2 == 5
